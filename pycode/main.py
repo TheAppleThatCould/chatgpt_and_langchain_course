19df89a329b372a9be30a088a04cadc5cdcbf9c9
Joshua Adams <joshuaadams031999@gmail.com>
2025-07-08 20:56:05 +1000

    Finish creating small application to chain to llm and return two outputs

--- pycode/main.py
+++ pycode/main.py
@@ -1,6 +1,6 @@
 from langchain_openai import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from dotenv import load_dotenv
 import argparse
 
@@ -18,15 +18,34 @@ code_prompt = PromptTemplate(
     input_variables=["language", "task"]
 )
 
-code_chain = LLMChain(
-    llm=llm,
-    prompt=code_prompt
+test_prompt = PromptTemplate(
+    input_variables=["language", "code"],
+    template="Write a test for the following {language} code:\n{code}"
 )
 
-result = code_chain({
+code_chain = LLMChain(
+    llm=llm,
+    prompt=code_prompt,
+    output_key="code"
+)
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key="test"
+)
+
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["task", "language"],
+    output_variables=["test", "code"]
+)
+
+result = chain({
     "language": args.language,
     "task": args.task
 })
 
-
-print(result["text"])
+print("\n >>>>>>>> GENERATED CODE")
+print(result["test"])
+print("\n >>>>>>>> GENERATED TEST")
+print(result["code"])
