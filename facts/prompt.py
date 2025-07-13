23e544e90da0fad526663487bd2d95fdb609b751
Joshua Adams <joshuaadams031999@gmail.com>
2025-07-13 14:33:33 +1000

    Create embedding AI chat with ChromaDB

---
+++ facts/prompt.py
@@ -0,0 +1,29 @@
+from langchain.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI()
+embeddings = OpenAIEmbeddings()
+
+db = Chroma(
+    persist_directory="emb",
+    embedding_function=embeddings
+)
+
+retriever = db.as_retriever()
+
+chain = RetrievalQA.from_chain_type(
+    llm=chat,
+    retriever=retriever,
+    chain_type="stuff"
+)
+
+
+result = chain.run("What is the longest word in the english language?")
+
+
+print(result)
